fcf1c6e6347353128c3b95fc7627fac0c2c51410
Sam Friedman <samtx@outlook.com>
2017-09-16 16:31:42 -0500

    begin working problem 3.3

---
+++ hw3_3.py
@@ -0,0 +1,14 @@
+# HW 2, Problem 3.3
+
+import numpy as np
+from scipy.stats import gamma, poisson
+from scipy.special import gamma as gammafn
+import matplotlib.pyplot as plt
+
+def part_a():
+    y_a = np.array([12,9,12,14,13,13,15,8,15,6])
+    y_b = np.array([11,11,10,9,9,8,7,10,6,8,8,9,7])
+
+
+if __name__ == "__main__":
+    part_a()
