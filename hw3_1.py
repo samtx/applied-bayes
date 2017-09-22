c7ecf84299c281cdd46a1f51708973f2d9f517d4
Sam Friedman <samtx@outlook.com>
2017-09-22 13:17:50 -0500

    add plotting stuff

--- hw3_1.py
+++ hw3_1.py
@@ -3,12 +3,17 @@
 from scipy.stats import bernoulli, beta
 from scipy.special import gamma
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # Take 100 samples from bernoulli(theta)
 # assume sum(y)=57
 # Determine probabilities of theta =[0.0, 0.1, ..., 0.9, 1.0] and plot them
 # Compute Pr(Sum(y)=57 | theta)
+
+fname = 'hw3_1'
+imgFmt = 'png'
 
 def prob_sum57(theta):
     N = 100
@@ -32,15 +37,19 @@ def main():
         print(i, probs[i])
 
 def part_b():
+    fname = fname + '_b'
     probs = np.zeros(11)
     THETAS = np.arange(0,1.1,0.1)
     for i, theta in enumerate(THETAS):
         probs[i] = prob_sum57(theta)
+    fig = plt.figure()
     plt.plot(THETAS,probs)
-    plt.xlabel(r'$\theta$')
-    plt.title('3.1b')
-    # plt.ylabel(r'$Pr(\Sum y_i = 57 \mid \theta)$')
-    plt.show()
+    plt.xlabel(r'$\theta$', fontsize=22)
+    plt.title('3.1b', fontsize=22)
+    plt.ylabel(r'$Pr(\Sum y_i = 57 \mid \theta)$')
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.savefig(fname+'.'+imgFmt, format=imgFmt, dpi=300)
 
 def part_c():
     probs = np.zeros(11)
