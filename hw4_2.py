bd5daea7456a4723047c654cef8b6846008bd065
Sam Friedman <samfriedman@tamu.edu>
2017-10-13 00:48:33 +0000

    4.2c

--- hw4_2.py
+++ hw4_2.py
@@ -1,11 +1,11 @@
 # HW 4, Problem 4.2
 
 import numpy as np
-from scipy.stats import beta, binom, bernoulli, gamma
-from scipy.special import gamma as gammafn
-from scipy.special import gammaln
-from scipy.misc import comb
-import random
+from scipy.stats import gamma, poisson
+# from scipy.special import gamma as gammafn
+# from scipy.special import gammaln
+# from scipy.misc import comb
+# import random
 
 import matplotlib
 matplotlib.use('Agg')
@@ -19,11 +19,11 @@ labelFontSize = 16
 tickFontSize = 12
 titleFontSize = 14
 
-def lgamma(x):
-    return gammafn(np.log(x))
+# def lgamma(x):
+#     return gammafn(np.log(x))
 
-def combln(N,k):
-    return gammaln(N+1)-gammaln(N-k+1)-gammaln(k+1)
+# def combln(N,k):
+#     return gammaln(N+1)-gammaln(N-k+1)-gammaln(k+1)
 
 def part_a(fname=fname):
     fname = fname + '_a'
@@ -70,9 +70,43 @@ def part_b(fname=fname):
     plt.savefig(fname+'.'+imgFmt, format=imgFmt)
         
 
-def part_e(fname=fname):
-    pass
-
+def part_c(fname=fname):
+    fname = fname + '_c'
+    n = int(1e5)
+    # repeat part a
+    # y = 0.0
+    # for i in range(n):
+    #     thetaA = gamma.rvs(237, scale=1.0/20)
+    #     yA = poisson.rvs(thetaA)    
+    #     thetaB = gamma.rvs(125, scale=1.0/14)
+    #     yB = poisson.rvs(thetaB)
+    #     if (yB < yA):
+    #         y += 1.0
+    # print("Pr(yB < yA) = {:.5f}".format(y/float(n)))
+    
+    # repeat part b
+    n0_list = np.arange(1,250)
+    Pr = np.zeros(n0_list.size) 
+    for j, n0 in enumerate(n0_list):
+        y = 0.0
+        for i in range(n):
+            thetaA = gamma.rvs(237, scale=1.0/20)
+            yA = poisson.rvs(thetaA)  
+            thetaB = gamma.rvs(12*n0, scale=1.0/n0)
+            yB = poisson.rvs(thetaB)  
+            if (yB < yA):
+                y += 1.0
+        Pr[j] = y/float(n)
+    plt.figure()
+    plt.plot(n0_list, Pr, lw=1.5)
+    plt.xlabel(r'$n_0$',fontsize=labelFontSize)
+    plt.ylabel(r'$Pr(\tilde{Y}_B < \tilde{Y}_A \mid y_A, y_B)$',fontsize=labelFontSize)
+    plt.title('4.2c',fontsize=titleFontSize)
+    plt.xticks(fontsize=tickFontSize)
+    plt.yticks(fontsize=tickFontSize)
+    plt.savefig(fname+'.'+imgFmt, format=imgFmt)
+    
+    
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         part_a()
@@ -81,24 +115,24 @@ if __name__ == "__main__":
         if sys.argv[1] == 'a':
             print('part a...')
             part_a()
-        elif sys.argv[1] == 'a1':
-            print('part a1...')
-            part_a1()
-        elif sys.argv[1] == 'a2':
-            print('part a2...')
-            part_a2()
-        elif sys.argv[1] == 'a3':
-            print('part a3...')
-            part_a3()
+        # elif sys.argv[1] == 'a1':
+        #     print('part a1...')
+        #     part_a1()
+        # elif sys.argv[1] == 'a2':
+        #     print('part a2...')
+        #     part_a2()
+        # elif sys.argv[1] == 'a3':
+        #     print('part a3...')
+        #     part_a3()
         elif sys.argv[1] == 'b':
             print('part b...')
             part_b()
         elif sys.argv[1] == 'c':
             print('part c...')
             part_c()
-        elif sys.argv[1] == 'd':
-            print('part d...')
-            part_d()
-        elif sys.argv[1] == 'e':
-            print('part e...')
-            part_e()
+        # elif sys.argv[1] == 'd':
+        #     print('part d...')
+        #     part_d()
+        # elif sys.argv[1] == 'e':
+        #     print('part e...')
+        #     part_e()
