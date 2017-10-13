4d146c5790e068c7bc722c2101855a82a1b9f164
Sam Friedman <samtx@outlook.com>
2017-10-12 19:51:06 -0500

    3.8

--- hw3_8.py
+++ hw3_8.py
@@ -1,7 +1,7 @@
 # HW 3, Problem 3.8
 
 import numpy as np
-from scipy.stats import beta, binom
+from scipy.stats import beta, binom, bernoulli
 from scipy.special import gamma as gammafn
 from scipy.special import gammaln
 from scipy.misc import comb
@@ -20,10 +20,10 @@ titleFontSize = 14
 
 def lgamma(x):
     return gammafn(np.log(x))
-    
+
 def combln(N,k):
     return gammaln(N+1)-gammaln(N-k+1)-gammaln(k+1)
-    
+
 def part_a(fname=fname):
     fname = fname + '_a'
     # x = 20
@@ -45,7 +45,7 @@ def part_a(fname=fname):
     plt.savefig(fname+'.'+imgFmt, format=imgFmt)
     print('mean = '+ str(np.mean(P)))
     print('stdv = '+ str(np.std(P)))
-    
+
     fig = plt.figure()
     plt.plot(x, 0.2*P1+0.4*P2+0.4*P3, lw=1.5)
     plt.xlabel(r'$\theta$',fontsize=labelFontSize)
@@ -55,8 +55,56 @@ def part_a(fname=fname):
     plt.yticks(fontsize=tickFontSize)
     # plt.legend()
     plt.savefig(fname+'2.'+imgFmt, format=imgFmt)
-    
-        
+
+
+def part_c(fname=fname):
+    fname = fname + '_c'
+    # x = 20
+    x = np.linspace(0,1,100)
+    y = 24
+    n = 50
+    fig = plt.figure()
+    def prior(x):
+        return 0.2*beta.pdf(x,4,4)+0.4*beta.pdf(x,4,2)+0.4*beta.pdf(x,2,4)
+    def likelihood(x,y,n):
+        return (x**y)*(1-x)**(n-y)
+    P = prior(x)*likelihood(x,y,n)
+    plt.plot(x, P, lw=1.5)
+    # plt.plot(x, 0.2*P1+0.4*P2+0.4*P3, lw=1.5)
+    plt.xlabel(r'$\theta$',fontsize=labelFontSize)
+    plt.ylabel(r'$p(\theta \mid y)$',fontsize=labelFontSize)
+    plt.title('3.8c',fontsize=titleFontSize)
+    plt.xticks(fontsize=tickFontSize)
+    plt.yticks(fontsize=tickFontSize)
+    # plt.legend()
+    plt.savefig(fname+'.'+imgFmt, format=imgFmt)
+    print('mean = '+ str(np.mean(P)))
+    print('stdv = '+ str(np.std(P)))
+
+
+def part_d(fname=fname):
+    fname = fname + '_d'
+    # x = 20
+    x = np.linspace(0,1,100)
+    y = 23
+    n = 50
+    fig = plt.figure()
+    def prior(x):
+        return 0.2*beta.pdf(x,4,4)+0.4*beta.pdf(x,4,2)+0.4*beta.pdf(x,2,4)
+    def likelihood(x,y,n):
+        return (x**y)*(1-x)**(n-y)
+    P = prior(x)*likelihood(x,y,n)
+    plt.plot(x, P, lw=1.5)
+    # plt.plot(x, 0.2*P1+0.4*P2+0.4*P3, lw=1.5)
+    plt.xlabel(r'$\theta$',fontsize=labelFontSize)
+    plt.ylabel(r'$p(\theta \mid y)$',fontsize=labelFontSize)
+    plt.title('3.8d',fontsize=titleFontSize)
+    plt.xticks(fontsize=tickFontSize)
+    plt.yticks(fontsize=tickFontSize)
+    # plt.legend()
+    plt.savefig(fname+'.'+imgFmt, format=imgFmt)
+    print('mean = '+ str(np.mean(P)))
+    print('stdv = '+ str(np.std(P)))
 
 def part_e(fname=fname):
     pass
@@ -65,7 +113,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         part_a()
     elif len(sys.argv) > 1:
-        
+
         if sys.argv[1] == 'a':
             print('part a...')
             part_a()
